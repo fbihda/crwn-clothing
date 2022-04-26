7028ad7b4495f061a6997ba835ff1eb22f3598c7
fbihda <fbihda@gmail.com>
2022-04-26 16:17:15 +0200

    adding cart context and shopping icon

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,20 @@
+import "./product-card.styles.scss";
+import Button from "../button/button.component";
+import './product-card.styles.scss'
+
+const ProductCard = ({product}) => {
+    const {name, price, imageUrl} = product;
+
+  return (
+    <div className="product-card-container">
+      <img src={imageUrl} alt={`${name}`}/>
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <Button buttonTapye='inverted'>Add to card</Button>
+    </div>
+  );
+};
+
+export default ProductCard;
